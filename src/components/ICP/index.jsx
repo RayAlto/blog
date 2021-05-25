e3410bbbbfc06e9a8a17c831b42d838008b9b177
RayAlto <tianjiayu668@gmail.com>
2021-05-25 14:18:08 +0800

    Make ICP looks more material

--- src/components/ICP/index.jsx
+++ src/components/ICP/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import withStyles from "@material-ui/core/styles/withStyles";
-import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import VerifiedUserRoundedIcon from "@material-ui/icons/VerifiedUserRounded";
 
@@ -14,23 +13,16 @@ class ICP extends React.Component {
 
   render() {
     return (
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
+      <Button
+        href={this.props.ipcLink}
+        target="_blank"
+        variant="text"
+        color="secondary"
+        className={this.props.classes.button}
+        startIcon={<VerifiedUserRoundedIcon />}
       >
-        <Button
-          href={this.props.ipcLink}
-          target="_blank"
-          variant="text"
-          color="secondary"
-          className={this.props.classes.button}
-          startIcon={<VerifiedUserRoundedIcon />}
-        >
-          {this.props.children}
-        </Button>
-      </Grid>
+        {this.props.children}
+      </Button>
     )
   }
 }
@@ -38,7 +30,7 @@ class ICP extends React.Component {
 export default withStyles(theme => ({
 
   button: {
-    margin: theme.spacing(1),
+    width: "100%",
   },
 
 }))(ICP);
