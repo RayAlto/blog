aa8f01ef4480535ed50b176d65281e103d4dd56e
RayAlto <tianjiayu668@gmail.com>
2021-06-25 08:53:12 +0800

    achieve integration with router with Tag

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
+import { Link } from "react-router-dom";
+
 import Chip from "@material-ui/core/Chip";
 
 export default class Tag extends React.Component {
@@ -9,20 +11,45 @@ export default class Tag extends React.Component {
   static propTypes = {
     href: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    isRouteLink: PropTypes.bool.isRequired,
+  };
+
+  static defaultProps = {
+    isRouteLink: false,
   };
 
   render() {
+
+    const {
+      href,
+      title,
+      isRouteLink,
+    } = this.props;
+
     return (
-      <Chip
-        variant="outlined"
-        color="primary"
-        size="small"
-        label={this.props.title}
-        component="a"
-        clickable
-        {...this.props}
-      />
+      isRouteLink ? (
+        <Chip
+          variant="outlined"
+          color="primary"
+          size="small"
+          label={title}
+          component={Link}
+          clickable
+          to={href}
+        />
+      ) : (
+        <Chip
+          variant="outlined"
+          color="primary"
+          size="small"
+          label={title}
+          component="a"
+          clickable
+          href={href}
+        />
+      )
     );
+
   };
 
 };
